670652efb90b708aaac1b6a24f3d398ef68b49d8
Podtopkin Alexei <podtopkinalexei@gmail.com>
2025-11-27 19:45:40 +0300

    urls reservations/api

---
+++ reservations/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import TableViewSet, ReservationViewSet
+
+router = DefaultRouter()
+router.register(r'tables', TableViewSet)
+router.register(r'reservations', ReservationViewSet, basename='reservation')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
